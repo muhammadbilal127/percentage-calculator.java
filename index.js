57891767e183cb1f973606f06770d79727362fa0
muhammadbilal127 <mbilal52257@gmail.com>
2024-12-27 12:12:31 -0800

    percentage

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+var username = prompt("enter your name")
+
+alert("thanks to open my website" + " " + username)
+
+var num = prompt("enter your otained mark")
+var num1 = prompt("enter your total mark")
+var newnumber  =  num / num1;
+var total = newnumber * 100;
+var round = Math.round(total);
+alert(round + "%");
+
+//  375 obtained mark total mark 500 = 75%
+
